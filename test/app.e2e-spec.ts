bd05243732f837919621fc403499cb716002d8c3
Laercio Nogueira <laercio.nogueira@rw3tecnologia.com>
2024-11-20 10:10:44 -0300

    test: create test validate /movies/intervals

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/modules/app.module';
+import { MoviesModule } from '../src/modules/movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import dataSource from '../src/database/typeorm.config';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        MoviesModule,
+        TypeOrmModule.forRootAsync({
+          useFactory: async () => ({
+            ...dataSource.options,
+          }),
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/movies/intervals (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/movies/intervals')
       .expect(200)
-      .expect('Hello World!');
+      .expect({
+        min: [
+          {
+            producer: 'Bo Derek',
+            interval: 6,
+            previousWin: 1984,
+            followingWin: 1990,
+          },
+        ],
+        max: [
+          {
+            producer: 'Matthew Vaughn',
+            interval: 13,
+            previousWin: 2002,
+            followingWin: 2015,
+          },
+        ],
+      });
   });
 });
